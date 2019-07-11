dc71f28cbb70ae2dd408a0e8ae95cb3afab40ffa
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-11 15:12:57 -0300

    'Hello World' message with String and Bean.

--- src/main/java/com/udemy/rest/microservices/controller/HelloWorldController.java
+++ src/main/java/com/udemy/rest/microservices/controller/HelloWorldController.java
@@ -3,11 +3,18 @@ package com.udemy.rest.microservices.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udemy.rest.microservices.model.HelloWorldBean;
+
 @RestController
 public class HelloWorldController extends AbstractController{
 
 	@GetMapping(path="/helloWorld")
 	public String helloWorld() {
 		return "Hello World.";
+	}
+	
+	@GetMapping(path="/helloWorldBean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World.");
 	}
 }
