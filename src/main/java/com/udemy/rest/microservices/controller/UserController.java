0c6db4cf162edb543bd40e3be60e456bb0818b14
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-12 16:11:48 -0300

    Returning 201 http code, wich means CREATED, after create a new user.

--- src/main/java/com/udemy/rest/microservices/controller/UserController.java
+++ src/main/java/com/udemy/rest/microservices/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.udemy.rest.microservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.rest.microservices.dao.UserDao;
 import com.udemy.rest.microservices.model.User;
@@ -19,14 +23,20 @@ public class UserController{
 	@Autowired
 	private UserDao userDao;
 
-	@GetMapping(path="/")
+	@GetMapping(path="")
 	public List<User> findAll(){
 		return userDao.findAll();
 	}
 	
-	@PostMapping(path="/")
-	public void save(@PathVariable User user){
-		userDao.save(user);
+	@PostMapping(path="")
+	public ResponseEntity<User> save(@RequestBody User user){
+		User savedUser = userDao.save(user);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 	
 	@GetMapping(path="/{id}")
--- src/main/java/com/udemy/rest/microservices/model/User.java
+++ src/main/java/com/udemy/rest/microservices/model/User.java
@@ -6,6 +6,9 @@ public class User {
 	private Integer id;
 	private String name;
 	private Date birthDate;
+	
+	public User(){
+	}
 
 	public User(Integer id, String name, Date birthDate) {
 		super();
