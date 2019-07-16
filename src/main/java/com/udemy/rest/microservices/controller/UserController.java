04ceb436ab44cf8a46c5bf026aaa6574ef0593d7
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-16 17:15:19 -0300

    Implementing the HATEOAS concept and put validations on User bean.

--- src/main/java/com/udemy/rest/microservices/controller/UserController.java
+++ src/main/java/com/udemy/rest/microservices/controller/UserController.java
@@ -3,7 +3,12 @@ package com.udemy.rest.microservices.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.core.DummyInvocationUtils;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,42 +24,54 @@ import com.udemy.rest.microservices.exception.UserNotFoundException;
 import com.udemy.rest.microservices.model.User;
 
 @RestController
-@RequestMapping(value="/users")
-public class UserController{
-	
+@RequestMapping(value = "/users")
+public class UserController {
+
 	@Autowired
 	private UserDao userDao;
 
 	@GetMapping()
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return userDao.findAll();
 	}
-	
+
 	@PostMapping()
-	public ResponseEntity<User> save(@RequestBody User user){
+	public ResponseEntity<User> save(@Valid @RequestBody User user) {
 		User savedUser = userDao.save(user);
-		URI location = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(savedUser.getId())
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
 				.toUri();
 		return ResponseEntity.created(location).build();
 	}
-	
-	@GetMapping(path="/{id}")
-	public User findBy(@PathVariable int id){
+
+	@GetMapping(path = "/{id}")
+	public Resource<User> findBy(@PathVariable int id) {
 		User user = userDao.findBy(id);
-		if(user == null){
-			throw new UserNotFoundException("User not found for id: "+ id);
+		if (user == null) {
+			throw new UserNotFoundException("User not found for id: " + id);
 		}
-		return user;
+
+		// HATEOAS
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = ControllerLinkBuilder
+				.linkTo(DummyInvocationUtils.methodOn(this.getClass()).findBy(id));
+		resource.add(linkTo.withRel("useById"));
+		
+		return resource;
 	}
-	
-	@DeleteMapping(path="/{id}")
-	public void deleteBy(@PathVariable int id){
+
+	@DeleteMapping(path = "/{id}")
+	public Resource deleteBy(@PathVariable int id) {
 		User deletedUser = userDao.deleteBy(id);
-		if(deletedUser == null){
-			throw new UserNotFoundException("User not found for id: "+ id);
+		if (deletedUser == null) {
+			throw new UserNotFoundException("User not found for id: " + id);
 		}
+
+		// HATEOAS
+		Resource resource = new Resource<User>(deletedUser);
+		ControllerLinkBuilder linkTo = ControllerLinkBuilder
+				.linkTo(DummyInvocationUtils.methodOn(this.getClass()).deleteBy(id));
+		resource.add(linkTo.withRel("deleteById"));
+
+		return resource;
 	}
 }
--- src/main/java/com/udemy/rest/microservices/model/User.java
+++ src/main/java/com/udemy/rest/microservices/model/User.java
@@ -2,12 +2,19 @@ package com.udemy.rest.microservices.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	private Integer id;
+
+	@Size(min = 2, message="The name should have at least 2 characters.")
 	private String name;
-	private Date birthDate;
 	
-	public User(){
+	@Past(message="Birthdate should be in the past.")
+	private Date birthDate;
+
+	public User() {
 	}
 
 	public User(Integer id, String name, Date birthDate) {
