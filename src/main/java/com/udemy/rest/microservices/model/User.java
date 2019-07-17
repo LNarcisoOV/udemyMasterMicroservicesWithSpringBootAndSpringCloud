45f40147ba20df5353ea24f83ecba717ab4c0f80
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-17 13:40:16 -0300

    Enhancing Swagger documentation with custom annotations.

--- src/main/java/com/udemy/rest/microservices/model/User.java
+++ src/main/java/com/udemy/rest/microservices/model/User.java
@@ -5,13 +5,19 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about the User.")
 public class User {
 	private Integer id;
 
 	@Size(min = 2, message="The name should have at least 2 characters.")
+	@ApiModelProperty(notes="The Name should have at least 2 characters.")
 	private String name;
 	
 	@Past(message="Birthdate should be in the past.")
+	@ApiModelProperty(notes="Birth date should be in the past.")
 	private Date birthDate;
 
 	public User() {
