a1f9d25eaa1bd7b83bd87c6f2f4092e738ddeec2
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-17 10:59:54 -0300

    Getting locale by the current location of the user.

--- src/main/java/com/udemy/rest/microservices/controller/HelloWorldController.java
+++ src/main/java/com/udemy/rest/microservices/controller/HelloWorldController.java
@@ -1,39 +1,37 @@
 package com.udemy.rest.microservices.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.rest.microservices.model.HelloWorldBean;
 
 @RestController
-public class HelloWorldController{
-	
+public class HelloWorldController {
+
 	@Autowired
 	private MessageSource messageSource;
 
-	@GetMapping(path="/helloWorld")
+	@GetMapping(path = "/helloWorld")
 	public String helloWorld() {
 		return "Hello World.";
 	}
-	
-	@GetMapping(path="/helloWorldBean")
+
+	@GetMapping(path = "/helloWorldBean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World.");
 	}
-	
-	@GetMapping(path="/helloWorldBean/{name}")
+
+	@GetMapping(path = "/helloWorldBean/{name}")
 	public HelloWorldBean helloWorldBeanWithName(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s.", name));
 	}
-	
-	@GetMapping(path="/goodMorningInternationalized")
-	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+
+	@GetMapping(path = "/goodMorningInternationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
