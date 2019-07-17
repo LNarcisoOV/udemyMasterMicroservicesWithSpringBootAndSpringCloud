3ad7dad45d26c935fd6fbe6f114a9d4e7423e8d1
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-17 10:47:41 -0300

    Internationalization example for messages added.

--- src/main/java/com/udemy/rest/microservices/UdemyMasterMicroservicesWithSpringBootAndSpringCloudApplication.java
+++ src/main/java/com/udemy/rest/microservices/UdemyMasterMicroservicesWithSpringBootAndSpringCloudApplication.java
@@ -1,7 +1,13 @@
 package com.udemy.rest.microservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class UdemyMasterMicroservicesWithSpringBootAndSpringCloudApplication {
@@ -9,5 +15,18 @@ public class UdemyMasterMicroservicesWithSpringBootAndSpringCloudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyMasterMicroservicesWithSpringBootAndSpringCloudApplication.class, args);
 	}
-
+	
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource blundleMessageResource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
